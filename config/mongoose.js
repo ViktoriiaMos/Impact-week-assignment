b2b7705be5ce6c32c190c2ddb3eaffef56efa519
maxlohvinov <113803763+maxlohvinov@users.noreply.github.com>
2022-11-27 13:48:29 +0100

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Viktoriia:Viktoriia@cluster0.ionvdvy.mongodb.net/?retryWrites=true&w=majority')
-//'mongodb+srv://Maksym_Lohvinov:Max_240208@@cluster0.ionvdvy.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Maksym_Lohvinov:Max_240208@cluster0.ionvdvy.mongodb.net/?retryWrites=true&w=majority')
 
     .then( () => console.log('DB connected'))
     .catch( err => console.log(err))
--- config/router.js
+++ config/router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const controller = require('../controllers/controllers')
 
 router.get('/', controller.getHomePage);
-router.get('/addQuestion', controller.getAddQuestionPage)
-router.get('/auth', controller.getLogInPage)
+router.get('/addQuestion', controller.getAddQuestionPage);
+router.get('/auth', controller.getLogInPage);
+router.get('/signup',controller.getSignUpPage);
+
 
 
 module.exports = router;
