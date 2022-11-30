b727fdb477f87fb0e0af7e32bd75334d61ec88c2
Viktoriia Moskalenko <v.moskalenkoda@gmail.com>
2022-11-30 23:52:38 +0100

    add auth, log out, keep working on comments

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const commentSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Question'
+    },
+    username: {
+        type: String
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model('Comment', commentSchema)
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const router = require('./config/router');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
 
 app.use('/public', express.static('public'));
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 
 require('./config/mongoose')
 
-app.use(router)
+
 
 app.listen(4200, ()=> console.log('Running on 4200'))
+
+// routes
+app.get('*', checkUser);
+app.get('/addQuestion', requireAuth, (req, res) => res.render('addQuestion'));
+app.use(router)
