43b6f9811d4fce5b7a40efde5a947c33e6fc07c6
Viktoriia Moskalenko <v.moskalenkoda@gmail.com>
2022-11-30 13:47:02 +0100

    error fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./config/router');
 
-app.use(express.static('public'));
+app.use('/public', express.static('public'));
 
 app.set('view engine', 'ejs');
 
