7bc6bbaf73bb25da22463d73ba8871132b5b8842
Viktoriia Moskalenko <v.moskalenkoda@gmail.com>
2022-12-01 13:21:17 +0100

    comments are not finished, edit-question error

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
-
-
 const commentSchema = new mongoose.Schema({
-    comment: {
-        type: String,
-        required: true
-    },
-    questionId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Question'
-    },
-    username: {
-        type: String
-    }
-},{timestamps: true})
+    author: String,
+    comment: String
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+// const commentSchema = new mongoose.Schema({
+//     comment: {
+//         type: String,
+//         required: true
+//     },
+//     questionId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Question'
+//     },
+//     email: {
+//         type: String
+//     }
+// },{timestamps: true})
 
 module.exports = mongoose.model('Comment', commentSchema)
